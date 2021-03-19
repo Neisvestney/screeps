0342e90bd62de84bbda000f1fa5f0410fd674af2
Neisvestney <neisvestney113@gmail.com>
2021-03-19 08:13:13 +0300

    Fix miner action

--- action.mine-and-transfer.js
+++ action.mine-and-transfer.js
@@ -3,7 +3,7 @@ const mine = require("action.mine");
 module.exports = {
     /** @param {Creep} creep **/
     do: function (creep) {
-        const targets = creep.pos.findInRange(FIND_MY_CREEPS, 1, { filter: (c) => c.store.getFreeCapacity() > 0});
+        const targets = creep.pos.findInRange(FIND_MY_CREEPS, 1, { filter: (c) => c.store.getFreeCapacity() > 0 && c.memory['role'] !== 'miner'});
 
         if (targets.length > 0 && creep.store.getFreeCapacity()/creep.store.getCapacity() < 0.2) {
             creep.transfer(targets[0], RESOURCE_ENERGY);
